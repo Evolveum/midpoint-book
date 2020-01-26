b1681fc645d17bda859b21b092f51f28a6ade3eb
Radovan Semancik <radovan.semancik@evolveum.com>
2020-01-26 19:55:16 +0100

    doc

--- build.sh
+++ build.sh
@@ -11,7 +11,11 @@ echo "### BUILDING HTML (single page)"
 asciidoctor -o $TARGET_DIR/$BOOK_NAME.html master.adoc
 
 #echo "### BUILDING HTML (chunked)"
+
+# Simple, obsolete
 #asciidoctor -r ./extensions/multipage-html5-converter.rb -b multipage_html5 -o $TARGET_DIR/$BOOK_NAME.html master.adoc
+
+# Does not work with 2.0, See https://github.com/owenh000/asciidoctor-multipage
 #asciidoctor -r ./extensions/asciidoctor-multipage.rb -b multipage_html5 -o target/midpoint-book.html master.adoc
 
 echo "### COPY IMAGES"
